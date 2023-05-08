74915f29a055769303a20b6158e9aab6fab0820e
Andrey <84859209+damaloonazhret@users.noreply.github.com>
2023-05-08 15:46:21 +0300

    refactor: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,44 +4,45 @@ const path = require('node:path');
 const directory = path.join(__dirname, 'files');
 const directoryCopy = path.join(__dirname, 'files-copy');
 
-async function copyDir (dir, copy){
-    try{
-        await fs.readdir(directoryCopy, {withFileTypes: true}, (err, files) => {
-            if (files == undefined){
-                return;
-            } else {
-                files.forEach((file) => {
-                    fs.unlink(path.join(directoryCopy, file.name), err => {
-                        if (err) {
-                            return err;
-                        };
-                    });
-                });
-            }
+async function copyDir(dir, copy) {
 
-        });
-    } finally {
-        fs.readdir(directory, {withFileTypes: true}, (err, files) => {
-            if (err) {
-                console.log(err);
-            } else {
-                fs.mkdir(directoryCopy, {recursive: true}, (err) => {
+    await fs.readdir(copy, {withFileTypes: true}, (err, files) => {
+        if (files == undefined) {
+            return;
+        } else {
+            files.forEach((file) => {
+                fs.unlink(path.join(copy, file.name), err => {
                     if (err) {
-                        console.log(err);
-                    } else {
-                        files.forEach((file) => {
-                            fs.copyFile(path.join(__dirname, 'files', file.name),
-                                path.join(__dirname, 'files-copy', file.name), (err) => {
-                                    if (err) {
-                                        console.log(err);
-                                    }
-                                });
-                        });
+                        return err;
                     }
+                    ;
                 });
-            }
-        });
-    }
+            });
+        }
+
+    });
+
+    fs.readdir(dir, {withFileTypes: true}, (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            fs.mkdir(copy, {recursive: true}, (err) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    files.forEach((file) => {
+                        fs.copyFile(path.join(__dirname, 'files', file.name),
+                            path.join(__dirname, 'files-copy', file.name), (err) => {
+                                if (err) {
+                                    console.log(err);
+                                }
+                            });
+                    });
+                }
+            });
+        }
+    });
+
 }
 
 copyDir(directory, directoryCopy);
