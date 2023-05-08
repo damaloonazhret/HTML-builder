86e892d9bc83286b925550671703abe8eaaea024
Andrey <84859209+damaloonazhret@users.noreply.github.com>
2023-05-08 19:07:35 +0300

    feat: 05-merge-styles check ext

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -9,8 +9,13 @@ const OUTPUT = fs.createWriteStream(bundleFolder);
 async function bundle(style) {
     const allStyles = await fs.promises.readdir(style, {withFileTypes: true});
     for (const currentStyle of allStyles) {
-        await fs.createReadStream(path.join(style, currentStyle.name))
-            .on('data', data => OUTPUT.write(data));
+        const N = currentStyle.name;
+        const position = N.indexOf('.');
+        const ext = N.slice(-(N.length - position) + 1);
+        if (ext === 'css') {
+            await fs.createReadStream(path.join(style, currentStyle.name))
+                .on('data', data => OUTPUT.write(data));
+        }
     }
 }
 
